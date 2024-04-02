22b632579a337b83053d37e5226de46a75a6d48f
Andreas Goodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-04-02 09:04:07 +0200

    solve day2

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
-mod day1;
+// mod day1;
 mod day2;
 
 use std::fs::read_to_string;
 use std::time::Instant;
 
 fn main() {
-    let input = read_lines("./src/examples/example2.txt");
+    let input = read_lines("./src/puzzles/puzzle2.txt");
 
     let now = Instant::now();
-    day2::solve1(input);
+    day2::solve2(input);
     let end = now.elapsed();
 
     println!("{:.2?}", end)
