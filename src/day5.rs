e34beb0d9e0c80088c24b95a45011d5174dd294a
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-04-15 16:12:46 +0200

    extract another function

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,8 @@
-use std::{cmp::min, ops::Range};
+use std::{
+    cmp::min,
+    collections::{HashMap, HashSet},
+    ops::Range,
+};
 
 struct Map {
     range: Range<isize>,
@@ -13,23 +17,31 @@ pub fn solve1(input: Vec<String>) {
 
     let map_list = parse_map_list(input_segments);
 
-    let mut min_location = isize::MAX;
-
-    for seed in seed_list {
-        let mut distance = seed;
-        for outer_map in &map_list {
-            for inner_map in outer_map {
-                if inner_map.range.contains(&distance) {
-                    distance = distance + inner_map.increment;
-                    break;
-                }
-            }
-        }
-        min_location = min(distance, min_location);
-    }
+    let min_location = find_min_location(seed_list, map_list);
 
     println!("{min_location}")
 }
+
+// pub fn solve2(input: Vec<String>) {
+//     let input_string = input.join("\n");
+//     let input_segments: Vec<&str> = input_string.split("\n\n").collect();
+
+//     let seed_range_list = parse_seed_list(input_segments[0]);
+
+//     let map_list = parse_map_list(input_segments);
+
+//     let mut seed_list: HashSet<isize> = HashSet::new();
+
+//     for index in (0..seed_range_list.len()).step_by(2) {
+//         for seed_index in 0..seed_range_list[index + 1] {
+//             seed_list.insert(seed_index + seed_range_list[index] as isize);
+//         }
+//     }
+
+//     let min_location = find_min_location(seed_list, map_list);
+
+//     println!("{min_location}")
+// }
 
 fn parse_seed_list(input: &str) -> Vec<isize> {
     let seed_segment = input.replace("seeds: ", " ");
@@ -78,4 +90,33 @@ fn parse_map_list(segments: Vec<&str>) -> Vec<Vec<Map>> {
     }
 
     map_list
+}
+
+fn find_min_location(seed_list: Vec<isize>, map_list: Vec<Vec<Map>>) -> isize {
+    let mut min_location = isize::MAX;
+    let mut location_map: HashMap<isize, isize> = HashMap::new();
+
+    for seed in seed_list {
+        let known_location = location_map.get(&seed);
+        if known_location.is_some() {
+            min_location = min(min_location, *known_location.unwrap());
+            continue;
+        }
+
+        let mut location = seed;
+        for outer_map in &map_list {
+            for inner_map in outer_map {
+                if inner_map.range.contains(&location) {
+                    location = location + inner_map.increment;
+                    break;
+                }
+            }
+        }
+
+        location_map.insert(seed, location);
+
+        min_location = min(location, min_location);
+    }
+
+    min_location
 }
