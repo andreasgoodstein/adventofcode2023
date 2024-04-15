25e9644d6d241785d8c43236dc1a55de729b476f
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-04-15 14:39:03 +0200

    solve day 5 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 // mod day1;
 // mod day2;
 // mod day3;
-mod day4;
+// mod day4;
+mod day5;
 
 use std::fs::read_to_string;
 use std::time::Instant;
 
 fn main() {
-    let input = read_lines("./src/inputs/puzzle4.txt");
+    let input = read_lines("./src/inputs/puzzle5.txt");
 
     let now = Instant::now();
-    day4::solve2(input);
+    day5::solve1(input);
     let end = now.elapsed();
 
     println!("{:.2?}", end)
