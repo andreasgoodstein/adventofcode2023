3898b78d5c7faf2daadd5e18e89f65792ff5b8cf
andreasgoodstein <24245735+andreasgoodstein@users.noreply.github.com>
2024-04-15 14:50:39 +0200

    extract to sub functions

--- src/day5.rs
+++ src/day5.rs
@@ -9,18 +9,44 @@ pub fn solve1(input: Vec<String>) {
     let input_string = input.join("\n");
     let input_segments: Vec<&str> = input_string.split("\n\n").collect();
 
-    let seed_segment = input_segments[0].replace("seeds: ", " ");
-    let seed_list: Vec<isize> = seed_segment
+    let seed_list = parse_seed_list(input_segments[0]);
+
+    let map_list = parse_map_list(input_segments);
+
+    let mut min_location = isize::MAX;
+
+    for seed in seed_list {
+        let mut distance = seed;
+        for outer_map in &map_list {
+            for inner_map in outer_map {
+                if inner_map.range.contains(&distance) {
+                    distance = distance + inner_map.increment;
+                    break;
+                }
+            }
+        }
+        min_location = min(distance, min_location);
+    }
+
+    println!("{min_location}")
+}
+
+fn parse_seed_list(input: &str) -> Vec<isize> {
+    let seed_segment = input.replace("seeds: ", " ");
+
+    seed_segment
         .trim()
         .split(' ')
         .filter(|number| !number.is_empty())
         .map(|seed| seed.parse::<isize>().unwrap())
-        .collect();
+        .collect()
+}
 
+fn parse_map_list(segments: Vec<&str>) -> Vec<Vec<Map>> {
     let mut map_list: Vec<Vec<Map>> = Vec::new();
 
-    for map_index in 1..input_segments.len() {
-        let map_split: Vec<&str> = input_segments[map_index].split("\n").collect();
+    for map_index in 1..segments.len() {
+        let map_split: Vec<&str> = segments[map_index].split("\n").collect();
 
         let mut new_map_list: Vec<Map> = Vec::new();
         for map_string in map_split {
@@ -51,20 +77,5 @@ pub fn solve1(input: Vec<String>) {
         map_list.push(new_map_list);
     }
 
-    let mut min_location = isize::MAX;
-
-    for seed in seed_list {
-        let mut distance = seed;
-        for outer_map in &map_list {
-            for inner_map in outer_map {
-                if inner_map.range.contains(&distance) {
-                    distance = distance + inner_map.increment;
-                    break;
-                }
-            }
-        }
-        min_location = min(distance, min_location);
-    }
-
-    println!("{min_location}")
+    map_list
 }
